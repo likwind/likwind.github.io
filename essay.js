421dbe156c82518c5b572c3ce68dfbf8348996de
likwind <nanhaijiao@outlook.com>
2017-06-18 04:34:28 +0800

    2.1
    for father‘s Day

---
+++ essay.js
@@ -0,0 +1,9 @@
+var title="第一天总结"；
+var essay="  今天，先看了敖天羽小姐姐的个人主页 空之领域 xsky.me ,觉得简洁明了，思路清晰，"+
+"决定仿照的做一个自己的网页来练练手，今天完成了最开始的导航栏部分，感觉还不错 ^.^。"+
+"要实现的东西还有很多，但就是在将遇到的问题一点点解决的过程中，自己就会慢慢进步。"+"<br>  厚积而薄发";
+
+var x=document.getElementById("textOne");
+x.document.getElementsByTagName("h1").innerHTML="title";
+var y=x.document.getElementsByTagName("p");
+y[0].innerHTML="essay";
